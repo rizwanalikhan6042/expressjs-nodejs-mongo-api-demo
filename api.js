ad5b592e602a0574f38f2a65ca1b0c5f1da92a0a
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2024-02-14 21:16:16 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const dbConnect=require('./mongo1Db');
 const app=express();
-
+// how to make a basic GET API with Mongo db and  Node js
 app.get('/',async (req,resp)=>{
     let data = await dbConnect();
     data=await data.find({name:"gufran Ali Khan"}).toArray();
